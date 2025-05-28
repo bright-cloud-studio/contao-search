f4aee5dffeebe420a47e22b8494bdfca1de0ae07
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-28 13:02:24 -0400

    Create tl_module.php

---
+++ src/Resources/contao/languages/en/tl_module.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Zyppy Search
+ *
+ * Copyright (C) 2019-2022 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/zyppy_search
+ * @link       https://andrewstevens.consulting
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_module']['ajaxTpl']  	 			= array('Ajax search results template', 'Here you can select the search results template returned via AJAX.');
+$GLOBALS['TL_LANG']['tl_module']['disableAjax'] 	 		= array('Disable AJAX', 'Disable Ajax search');
+$GLOBALS['TL_LANG']['tl_module']['formatPageTeaser']  		= array('Format Page Teaser', 'Strip tags and optionally limit characters for page teaser.');
+$GLOBALS['TL_LANG']['tl_module']['pageTeaserLimit']  		= array('Page Teaser Limit', 'Limit page teaser to this many characters (0 for unlimited).');
+$GLOBALS['TL_LANG']['tl_module']['formatPageDescription']  	= array('Format Page Description', 'Strip tags and optionally limit characters for page description.');
+$GLOBALS['TL_LANG']['tl_module']['pageDescriptionLimit']  	= array('Page Description Limit', 'Limit page description to this many characters (0 for unlimited).');
+$GLOBALS['TL_LANG']['tl_module']['formatNewsTeaser']  		= array('Format News Teaser', 'Strip tags and optionally limit characters for news teaser.');
+$GLOBALS['TL_LANG']['tl_module']['newsTeaserLimit']  		= array('News Teaser Limit', 'Limit news teaser to this many characters (0 for unlimited).');
