d5fbe70ba38848382bdea2ac69355e8a7199c566
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-28 13:02:12 -0400

    Create modules.php

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Zyppy Search
+ *
+ * Copyright (C) 2019-2022 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/zyppy_search
+ * @link       https://andrewstevens.consulting
+ */
+
+
+/**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD']['zyppy_search'] 				= array('Zyppy Search', 'Zyppy Live page search.');
