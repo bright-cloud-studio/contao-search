27c9eb6a03d74b949be1e08a8a09e329f3d0831b
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-28 12:58:45 -0400

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,13 @@
+
+<?php
+ 
+/**
+* @copyright  Bright Cloud Studio
+* @author     Bright Cloud Studio
+* @package    Contao CE Recaptcha
+* @license    LGPL-3.0+
+* @see	       https://github.com/bright-cloud-studio/contao-search
+*/
+
+/* Front End Module */
+$GLOBALS['FE_MOD']['application']['zyppy_search'] = 'ZyppySearch\Module\ZyppySearch';
