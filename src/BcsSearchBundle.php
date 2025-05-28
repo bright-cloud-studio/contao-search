f87597fdbe8483253e920795ae31efa23faa8e52
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-28 12:55:00 -0400

    Create BcsSearchBundle.php

---
+++ src/BcsSearchBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Glide
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-search
+ */
+
+namespace Bcs\SearchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsSearchBundle extends Bundle
+{
+}
