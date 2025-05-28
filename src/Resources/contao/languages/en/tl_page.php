408e635a215a54dbda5067241dba2d6b3b282c6c
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-28 13:02:44 -0400

    Create tl_page.php

---
+++ src/Resources/contao/languages/en/tl_page.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Zyppy Search
+ *
+ * Copyright (C) 2019-2022 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/zyppy_search
+ * @link       https://andrewstevens.consulting
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_page']['zyppy_news'] 	= array('News Reader', 'Override page image and teaser with news article.');
